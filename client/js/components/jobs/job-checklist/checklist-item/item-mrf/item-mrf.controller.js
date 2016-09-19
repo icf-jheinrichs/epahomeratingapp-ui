a13959cbb4a77065a4b5bc3596b75ed200ba1912
Jeff Heinrichs <jeff.heinrichs@icfi.com>
2016-09-19 12:31:40 -0500

    update controller for MRF item view.

--- client/js/components/jobs/job-checklist/checklist-item/item-mrf/item-mrf.controller.js
+++ client/js/components/jobs/job-checklist/checklist-item/item-mrf/item-mrf.controller.js
@@ -1,6 +1,33 @@
+const buttons = [
+    {
+        'display' : 'Rater Verified',
+        'value'   : 'rater-verified'
+    },
+    {
+        'display' : 'Must Correct',
+        'value'   : 'must-correct'
+    },
+    {
+        'display' : 'Builder Verified',
+        'value'   : 'builder-verified'
+    },
+    {
+        'display' : 'N/A',
+        'value'   : 'na'
+    }
+];
+
 class ChecklistItemMrfController {
     constructor () {
         'ngInject';
+    }
+
+    $onInit () {
+        this.responseButtons = this.getOptions();
+    }
+
+    getOptions () {
+        return buttons;
     }
 }
 
