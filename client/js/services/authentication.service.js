d6c09d613a55f74139ac9e181d2d17767a0f9473
alejandroq <quesadar@dukes.jmu.edu>
2017-01-01 14:13:19 -0500

    Promises for Local User Token with Congito User Token verfication

--- client/js/services/authentication.service.js
+++ client/js/services/authentication.service.js
@@ -1,5 +1,6 @@
-import Promise from 'bluebird';
+import AWS from 'aws-sdk';
 import angular from 'angular';
+import Promise from 'bluebird';
 import { CognitoUserPool, CognitoIdentityCredentials, CognitoUser, AuthenticationDetails } from 'amazon-cognito-identity-js';
 
 //TODO: finalize user data structure.
@@ -28,37 +29,51 @@ class AuthenticationService {
 
         this.$q                  = $q;
 
-        //TODO: refactor?
-        //try looking in session storage for user object, if null use default
-        try {
-            this.user = angular.fromJson(window.sessionStorage.getItem(USER_SESSION_ITEM)) || Object.assign({}, DEFAULT_USER);
-        } catch (err) {
-            this.user = Object.assign({}, DEFAULT_USER);
-        }
-
         this.userPool = new CognitoUserPool( POOL_DATA );
         this.cognitoUser = this.userPool.getCurrentUser();
-        if (this.cognitoUser != null) {
-            var auth = Promise.promisify(this.getSession.bind(this));
-            
-            // ERRORS OCCUR HERE
-            auth().then( function (res) {
-                this.userIsAuthenticated = this.compareTokens();
-                if (this.userIsAuthenticated) {
-                    this.userIDtoAWSCognitoCredentials();
-                    return Promise.resolve({
-                        'status' : 200
-                    });
-                } else {
-                    this.setUser({'status' : 403});
-                    return Promise.reject({
-                        'status' : 500
-                    });
-                }
-            })
-            .catch(console.log)
-            .bind(this);
-        }
+        this.authenticateLocalUser();
+    }
+
+    setToken (res) {
+        this.token = res['token'];
+        console.log(this.token);
+    }
+
+    authenticateLocalUser () {
+        var local = new Promise((resolve, reject) => {
+                resolve(angular.fromJson(window.sessionStorage.getItem(USER_SESSION_ITEM)));
+            });
+
+        var token = new Promise((resolve, reject) => {
+                    if (this.cognitoUser != null) {
+                        this.cognitoUser
+                            .getSession(function(err, session) {
+                                if (!err) {
+                                    resolve({'token' : session.getIdToken().getJwtToken()});
+                                }
+                                reject(err);
+                            });
+                    } else {
+                        reject(null);
+                    }
+                });
+
+        var iter = [ local, token.bind(this) ];
+
+        Promise.all(iter).then(results => {
+            var localUser    = results[0];
+            var cognitoToken = results[1];
+            if (localUser.token == cognitoToken) {
+                localUser.status = 200;
+            } else {
+                localUser.status = 403;
+            }
+            this.setUser(localUser);
+        })
+        .bind(this)
+        .catch(function(err) {
+            this.setUser({'status':403});
+        });
     }
 
     getUser () {
@@ -87,10 +102,10 @@ class AuthenticationService {
                 var auth = Promise.promisify(this.getSession).bind(this);
 
                 // ERRORS OCCUR HERE
-                auth().then(this.getAttributes)
-                .then(this.setUser)
-                .catch(console.log)
-                .bind(this);
+                // auth().then(this.getAttributes)
+                // .then(this.setUser)
+                // .catch(console.log)
+                // .bind(this);
 
                 return this.$q((resolve, reject) => {
                     resolve({
@@ -161,25 +176,12 @@ class AuthenticationService {
 
     logout () {
         this.setUser({'status' : '403'});
-        this.cognitoUser.signOut();
-
         return this.$q((resolve, reject) => {
             resolve({
                 message : 'success',
                 status  : 200
             });
         });
-    }
-
-    // compare local stored token to cognitouser at onset
-    // if different, when one is stored, then user 
-    compareTokens () {
-        if (this.user.token == this.token) {
-            return true;
-        } else {
-            this.token = '';
-            return false;
-        }
     }
 
     getAttributes (res) {
@@ -201,31 +203,23 @@ class AuthenticationService {
         }
     }
 
-    getSession () {
-        this.cognitoUser
-            .getSession(function(err, session) {
-                if (err) {
-                    return Promise.reject(err);
-                }
-                this.token = session.getIdToken().getJwtToken();
-                return Promise.resolve({
-                    'status' : 200
-                });
-            });
-    }
-
     setUser (attr) {
-        // TODO: attach token to user
-        if (attr['status'] != 403 || attr['status'] != 500) {
-            delete attr['status'];
-            attr['userId'] = this.cognitoUser.getUsername();
-            attr['token']  = this.token;
+        if (attr['status'] == 200) {
+            delete attr.status;
+            attr.userId = this.cognitoUser.getUsername();
+            attr.token  = this.token;
+            
             this.userIsAuthenticated = true;
             this.user = Object.assign({}, attr);
             
             // TODO: Is there a more secure way to store persistant login?
             window.sessionStorage.setItem(USER_SESSION_ITEM, angular.toJson(this.user));
         } else {
+            // if there is a signed on cognitouser but a disagreeable 
+            // token - the cognitouser is signed out
+            if (this.cognitoUser != null) {
+                this.cognitoUser.signOut();
+            }
             this.userIsAuthenticated = false;
             this.user = Object.assign({}, DEFAULT_USER);
             window.sessionStorage.setItem(USER_SESSION_ITEM, angular.toJson(DEFAULT_USER));
@@ -234,7 +228,7 @@ class AuthenticationService {
 
     userIDtoAWSCognitoCredentials () {
         AWS.config.credentials = new AWS.CognitoIdentityCredentials({
-            IdentityPoolId : POOL_DATA.UserPoolId, // your identity pool id here
+            IdentityPoolId : POOL_DATA.UserPoolId,
             Logins : {
                 AWS_KEY : this.token
             }
