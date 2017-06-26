23d72d79239cebef5133bd16f275a547e1a912a4
Yuantong Lu <Yuantong.Lu@icf.com>
2017-06-26 10:04:05 -0400

    user have ability to edit job

--- client/js/pages/job-edit-page/job-edit-page.controller.js
+++ client/js/pages/job-edit-page/job-edit-page.controller.js
@@ -1,10 +1,70 @@
+const ERROR_SERVER = {
+    type        : 'error',
+    text        : 'There was an error processing your request. Please try again.',
+    dismissable : false
+};
+
+const SUCCESS = {
+    type        : 'success',
+    text        : 'Job Edited.',
+    dismissable : false
+};
+
+const HOUSE_PLAN_REQUIRED = {
+    type        : 'error',
+    text        : 'Please add a House Plan to all samples.',
+    dismissable : false
+};
+
 class JobsEditPageController {
-    constructor () {
+    constructor ($state, JobsService) {
         'ngInject';
+
+        this.$state = $state;
+        this.JobsService = JobsService;
+    }
+
+    $onInit () {
+        this.message = {};
+    }
+
+    housePlansAreValid (job) {
+        let housePlansValid = true;
+
+        if (job.Primary.HousePlan.length < 1) {
+            housePlansValid = false;
+        }
+
+        job.Secondary.forEach((location) => {
+            if (location.HousePlan.length < 1) {
+                housePlansValid = false;
+            }
+        });
+
+        return housePlansValid;
     }
 
     submitJob (job) {
-        angular.noop();
+        this.message = {};
+
+        if (this.housePlansAreValid(job)) {
+            this.isBusy = true;
+
+            this.JobsService
+                .put(job)
+                .then(response => {
+                    this.message = Object.assign({}, SUCCESS);
+                    this.$state.go('jobs');
+                })
+                .catch(error => {
+                    this.message = Object.assign({}, ERROR_SERVER);
+                })
+                .finally(() => {
+                    this.isBusy = false;
+                });
+        } else {
+            this.message = Object.assign({}, HOUSE_PLAN_REQUIRED);
+        }
     }
 }
 
