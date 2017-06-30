dc31c0716c43efac3158ee1a9253c8738ad288ea
Yuantong Lu <Yuantong.Lu@icf.com>
2017-06-30 14:02:42 -0400

    support select builder when create job

--- common-ui/js/components/common/file-manager/file-manager.component.js
+++ common-ui/js/components/common/file-manager/file-manager.component.js
@@ -6,6 +6,7 @@ let fileManagerComponent = {
         accept     : '@',
         label      : '@',
         uploadOnly : '@',
+        enabled    : '=',
         inputId    : '@',
         files      : '=',
         library    : '<'
--- common-ui/js/components/jobs/job-detail/job-detail-location/job-detail-location.controller.js
+++ common-ui/js/components/jobs/job-detail/job-detail-location/job-detail-location.controller.js
@@ -1,8 +1,28 @@
+import _find from 'lodash/find';
+import _filter from 'lodash/filter';
+
 class JobDetailLocationController {
     constructor (UI_ENUMS) {
         'ngInject';
 
         this.ratingTypeOptions = UI_ENUMS.RATING_TYPES;
+
+        this.housePlanForBuilder = [];
+        this.selectHousePlanEnabled = false;
+    }
+
+    builderChange (builderId) {
+        let builder = _find(this.housePlans.index, function compare (o) {
+            return o.builder === builderId;
+        });
+
+        let housePlans = builder.housePlan;
+
+        this.housePlanForBuilder = _filter(this.housePlans.housePlan, function filter (o) {
+            return housePlans.indexOf(o._id) !== -1;
+        });
+
+        this.selectHousePlanEnabled = this.housePlanForBuilder.length !== 0;
     }
 }
 
