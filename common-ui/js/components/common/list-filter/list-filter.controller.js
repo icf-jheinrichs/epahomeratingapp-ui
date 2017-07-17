bb70c58accec3f600d548d918d6ff086fc998e28
Yuantong Lu <Yuantong.Lu@icf.com>
2017-07-17 11:35:15 -0400

    support job page tab

--- common-ui/js/components/common/list-filter/list-filter.controller.js
+++ common-ui/js/components/common/list-filter/list-filter.controller.js
@@ -28,13 +28,23 @@ class ListFilterController {
 
     $onInit () {
         this.buttons  = buttons;
+        this.initialSelected = buttons[0];
     }
 
     switchPage (selected) {
+        // TODO: Make this thing general
         if (selected[0] === 'offline-jobs') {
             this
                 .$rootScope
                 .$emit(this.MESSAGING.REFRESH_JOBS_LIST, this.JOB_PAGE_TAB.OFFLINE_JOBS);
+        } else if (selected[0] === 'internal-review') {
+            this
+                .$rootScope
+                .$emit(this.MESSAGING.REFRESH_JOBS_LIST, this.JOB_PAGE_TAB.INTERNAL_REVIEW);
+        } else if (selected[0] === 'history') {
+            this
+                .$rootScope
+                .$emit(this.MESSAGING.REFRESH_JOBS_LIST, this.JOB_PAGE_TAB.HISTORY);
         } else if (selected[0] === 'active') {
             this
                 .$rootScope
