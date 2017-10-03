c6fd540f3272a632f2eebc938dc7e6ebaee5a72f
Yuantong Lu <Yuantong.Lu@icf.com>
2017-10-03 10:27:18 -0400

    bulk-upload

--- common-ui/js/components/house-plans/house-plan-new/house-plan-new.controller.js
+++ common-ui/js/components/house-plans/house-plan-new/house-plan-new.controller.js
@@ -1,6 +1,6 @@
 const ERROR_INPUT = {
     type        : 'error',
-    text        : 'Please select a single valid house plan. File extension must be .xml and file should be no larger than 200kb.',
+    text        : 'Please select valid house plan. File extension must be .xml and file should be no larger than 200kb.',
     dismissable : false
 };
 
@@ -9,6 +9,20 @@ const ERROR_SERVER = {
     text        : 'There was an error processing your request. Please try again.',
     dismissable : false
 };
+
+const UPLOAD_STAGE = {
+    select    : 'select',
+    uploading : 'uploading',
+    report    : 'report'
+};
+
+const FILE_STAGE = {
+    uploading : 'uploading',
+    uploaded  : 'uploaded',
+    error     : 'error'
+}
+
+import _isEmpty from 'lodash/isEmpty'
 
 class HousePlanNewController {
     constructor ($rootScope, $state, HousePlansService, UI_ENUMS) {
@@ -19,7 +33,10 @@ class HousePlanNewController {
 
         this.HousePlansService = HousePlansService;
         this.MESSAGING         = UI_ENUMS.MESSAGING;
-        this.isBusy            = false;
+
+        this.uploadStage        = UPLOAD_STAGE.select;
+        this.totalNumUpload     = 0;
+        this.uploadRes          = {};
     }
 
     $onInit () {
@@ -31,38 +48,84 @@ class HousePlanNewController {
     }
 
     onSubmit () {
-        let file = this.fileInput.files[0];
-        let formData;
+        console.log(this.fileInput.files);
 
-        this.message = {};
-
-        if (this.fileInput.files.length === 1 && this.isValidFile(file)) {
-            this.isBusy  = true;
-            formData = new window.FormData();
-
-            formData.append('filedata', file, file.name);
-
-            this.HousePlansService
-                .post(formData)
-                .then((response) => {
-                    if (response.code === 200 && response.data.docID) {
-                        this.$rootScope.$emit(this.MESSAGING.HOUSE_PLAN_NEW, response.data);
-
-                        this.$state.go('house-plans.edit', {id : response.data.docID});
-                    }
-                })
-                .catch((error) => {
-                    this.errorReason  = Object.assign({}, ERROR_SERVER);
-                    this.errorReason.text = error.reason;
-
-                    this.message = Object.assign({}, ERROR_SERVER);
-                })
-                .finally(() => {
-                    this.isBusy  = false;
-                });
-        } else {
+        if (_isEmpty(this.fileInput.files)) {
+            this.errorReason  = Object.assign({}, ERROR_INPUT);
             this.message = Object.assign({}, ERROR_INPUT);
+            return;
         }
+
+        this.uploadStage = UPLOAD_STAGE.uploading;
+
+        this.totalNumUpload = this.fileInput.files.length;
+
+        // for (let fileIndex in this.fileInput.files) {
+        //     let res = {
+        //         status  : FILE_STAGE.uploading,
+        //         message : ''
+        //     };
+        //
+        //     this.uploadRes[this.fileInput.files[fileIndex].name] = res;
+        // }
+
+        let uploadPromises = [];
+        let files = this.fileInput.files;
+        let self = this;
+
+        let uploadFile = function uploadFile (file) {
+            return new Promise((resolve, reject) => {
+                console.log(file);
+
+                if (!self.isValidFile(file)) {
+                    self.message = Object.assign({}, ERROR_INPUT);
+                    return;
+                }
+
+                let formData = new window.FormData();
+                formData.append('filedata', file, file.name);
+
+                console.log('ready to post file ' + file.name);
+                self.HousePlansService
+                    .post(formData)
+                    .then((response) => {
+                        console.log(response.data);
+                        if (response.code === 200 && response.data.docID) {
+                            console.log('uploaded ' + response.data.docID);
+                            resolve(response.data.docID);
+                        }
+                    })
+                    .catch((error) => {
+                        console.log('upload failed' + error.data.docID);
+                        console.log(error);
+
+                        // this.errorReason  = Object.assign({}, ERROR_SERVER);
+                        // this.errorReason.text = error.reason;
+                        //
+                        // this.message = Object.assign({}, ERROR_SERVER);
+                    })
+                    .finally(() => {
+                        console.log('finally');
+                    });
+            });
+        };
+
+        for (let fileIndex in files) {
+            console.log(fileIndex);
+            console.log('push' + files[fileIndex]);
+
+            let file = files[fileIndex];
+            if (!(file instanceof File)) {
+                continue;
+            }
+
+            uploadPromises.push(uploadFile(files[fileIndex]));
+        }
+
+        Promise.all(uploadPromises).then((response) => {
+            console.log('house plan all uploaded');
+            console.log(response);
+        });
     }
 }
 
