d1652f7f850ebb78f28781bb8c77eab9b1e7e9b1
YuantongL <Yuantong.Lu@icf.com>
2018-03-28 14:50:50 -0400

    disable toggle while offline

--- common-ui/js/components/common/toggle/toggle.component.js
+++ common-ui/js/components/common/toggle/toggle.component.js
@@ -6,6 +6,7 @@ let toggleComponent = {
         handleChange             : '&',
         toggleClass              : '<',
         toggleConfirmationDialog : '@',
+        toggleDisabledDialog     : '@',
         toggleId                 : '@',
         toggleValue              : '<'
     },
