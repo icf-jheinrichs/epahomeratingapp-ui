65e4ca1b8aa77ca275b7a9111d7d29070b004e1d
Jeff Heinrichs <jeff.heinrichs@icf.com>
2018-08-13 16:17:12 -0400

    adding new files

---
+++ common-ui/js/components/jobs/job-checklist/checklist-house-selection/house-elevation-photos/house-elevation-photos.controller.js
@@ -0,0 +1,20 @@
+class houseElevationPhotosController {
+    constructor (jobTitleFilter) {
+        'ngInject';
+
+        this.jobTitleFilter = jobTitleFilter;
+    }
+
+    handlePhotoCapture (photo) {
+        this.onUpdateHousePhoto({
+            HouseId : this.house.HouseId,
+            photo
+        });
+    }
+
+    get HouseTitle () {
+        return this.jobTitleFilter(this.house.AddressInformation);
+    }
+}
+
+export default houseElevationPhotosController;
