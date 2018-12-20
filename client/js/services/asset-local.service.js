45b63c409be087b584ad7a0b3d902e10e2e8c49a
hunterBhough <hunter.hough@gmail.com>
2018-12-20 14:13:33 -0500

    stubs added for local development. logic in phonegap

---
+++ client/js/services/asset-local.service.js
@@ -0,0 +1,13 @@
+
+//
+// Stub service - does custom logic in phonegap app
+//
+class AssetLocalService {
+
+
+    checkIfAssetAvailable (assetIdentifier) {
+        return true;
+    }
+}
+
+export default AssetLocalService;
