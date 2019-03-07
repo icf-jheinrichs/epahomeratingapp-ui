fca817fab893503679a4cc90d12b6cb038ee38bb
Saad Ashraf <saad.ashraf@icf.com>
2019-03-07 15:29:42 -0500

    Original solution stopped working, used filter this time

--- client/js/services/jobs.service.js
+++ client/js/services/jobs.service.js
@@ -15,6 +15,7 @@ class JobsService {
      *
      * @param  {function} $q        angular.$q promise providers
      * @param  {function} $http     angular.$http ajax requests
+     * @param  {function} $sce
      * @param  {function} $sanitize angular.$sanitize html injection
      * @param  {object}   API_URL   epahomeratingapp constants - contains paths to API
      */
@@ -636,53 +637,34 @@ class JobsService {
     }
 
     sanitize (job) {
-        console.log(this.trustAsHtml(job.Primary.Builder));
-
-        job.Primary.Builder                             = this.trustAsHtml(job.Primary.Builder);
-
-        job.Primary.AddressInformation.Address1         = this.trustAsHtml(job.Primary.AddressInformation.Address1);
-        job.Primary.AddressInformation.CityMunicipality = this.trustAsHtml(job.Primary.AddressInformation.CityMunicipality);
-        job.Primary.AddressInformation.CommunityName    = this.trustAsHtml(job.Primary.AddressInformation.CommunityName);
-        job.Primary.AddressInformation.LotNo            = this.trustAsHtml(job.Primary.AddressInformation.LotNo);
-        job.Primary.AddressInformation.ManualId         = this.trustAsHtml(job.Primary.AddressInformation.ManualId);
-        job.Primary.AddressInformation.StateCode        = this.trustAsHtml(job.Primary.AddressInformation.StateCode);
-        job.Primary.AddressInformation.ZipCode          = this.trustAsHtml(job.Primary.AddressInformation.ZipCode);
-        job.Primary.ExportFilename                      = this.trustAsHtml(job.Primary.ExportFilename);
+        job.Primary.Builder                             = this.sanitizeReplace(job.Primary.Builder);
+        job.Primary.AddressInformation.Address1         = this.sanitizeReplace(job.Primary.AddressInformation.Address1);
+        job.Primary.AddressInformation.CityMunicipality = this.sanitizeReplace(job.Primary.AddressInformation.CityMunicipality);
+        job.Primary.AddressInformation.CommunityName    = this.sanitizeReplace(job.Primary.AddressInformation.CommunityName);
+        job.Primary.AddressInformation.LotNo            = this.sanitizeReplace(job.Primary.AddressInformation.LotNo);
+        job.Primary.AddressInformation.ManualId         = this.sanitizeReplace(job.Primary.AddressInformation.ManualId);
+        job.Primary.AddressInformation.StateCode        = this.sanitizeReplace(job.Primary.AddressInformation.StateCode);
+        job.Primary.AddressInformation.ZipCode          = this.sanitizeReplace(job.Primary.AddressInformation.ZipCode);
+        job.Primary.ExportFilename                      = this.sanitizeReplace(job.Primary.ExportFilename);
 
         job.Secondary.forEach((secondary, index) => {
-            job.Secondary[index].Builder                             = this.trustAsHtml(job.Secondary[index].Builder);
-            job.Secondary[index].AddressInformation.Address1         = this.trustAsHtml(job.Secondary[index].AddressInformation.Address1);
-            job.Secondary[index].AddressInformation.CityMunicipality = this.trustAsHtml(job.Secondary[index].AddressInformation.CityMunicipality);
-            job.Secondary[index].AddressInformation.CommunityName    = this.trustAsHtml(job.Secondary[index].AddressInformation.CommunityName);
-            job.Secondary[index].AddressInformation.LotNo            = this.trustAsHtml(job.Secondary[index].AddressInformation.LotNo);
-            job.Secondary[index].AddressInformation.ManualId         = this.trustAsHtml(job.Secondary[index].AddressInformation.ManualId);
-            job.Secondary[index].AddressInformation.StateCode        = this.trustAsHtml(job.Secondary[index].AddressInformation.StateCode);
-            job.Secondary[index].AddressInformation.ZipCode          = this.trustAsHtml(job.Secondary[index].AddressInformation.ZipCode);
-            job.Secondary[index].ExportFilename                      = this.trustAsHtml(job.Secondary[index].ExportFilename);
+            job.Secondary[index].Builder                             = this.sanitizeReplace(job.Secondary[index].Builder);
+            job.Secondary[index].AddressInformation.Address1         = this.sanitizeReplace(job.Secondary[index].AddressInformation.Address1);
+            job.Secondary[index].AddressInformation.CityMunicipality = this.sanitizeReplace(job.Secondary[index].AddressInformation.CityMunicipality);
+            job.Secondary[index].AddressInformation.CommunityName    = this.sanitizeReplace(job.Secondary[index].AddressInformation.CommunityName);
+            job.Secondary[index].AddressInformation.LotNo            = this.sanitizeReplace(job.Secondary[index].AddressInformation.LotNo);
+            job.Secondary[index].AddressInformation.ManualId         = this.sanitizeReplace(job.Secondary[index].AddressInformation.ManualId);
+            job.Secondary[index].AddressInformation.StateCode        = this.sanitizeReplace(job.Secondary[index].AddressInformation.StateCode);
+            job.Secondary[index].AddressInformation.ZipCode          = this.sanitizeReplace(job.Secondary[index].AddressInformation.ZipCode);
+            job.Secondary[index].ExportFilename                      = this.sanitizeReplace(job.Secondary[index].ExportFilename);
         });
         return job;
     }
 
-    trustAsHtml (s) {
-        return this.$sce.trustAsHtml(s);
-    }
-
-
-    unsanitize (sanitizedString) {
-
-        console.log(sanitizedString);
-        var str = sanitizedString;
-        var ele = document.createElement('div');
-
-        // use setAttribute to set attributes
-        ele.setAttribute('name', str);
-        console.log(str);
-        console.log(ele);
-        // use textContent to set the content of the element
-        ele.textContent = str;
-        console.log(ele.textContent);
-        var unSanitizedString = ele.outerHTML;
-        return unSanitizedString;
+    sanitizeReplace(s) {
+        s = this.$sanitize(s);
+        s = s.replace('&amp;','&');
+        return s;
     }
 }
 
